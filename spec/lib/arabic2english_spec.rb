a01e3b3f983d67e056f25bff06500b3572370a29
tos-miyake <tosihiro0917@gmail.com>
2016-04-06 22:33:22 +0900

    add base spec

--- spec/lib/arabic2english_spec.rb
+++ spec/lib/arabic2english_spec.rb
@@ -1,5 +1,9 @@
 require 'arabic2english'
 
 describe Arabic2English do
-  it "to_english"
+  it { expect(1).to respond_to(:to_english) }
+
+  it { expect(1.to_english).to eq('one') }
+  it { expect(11.to_english).to eq('eleven') }
+  it { expect(100.to_english).to eq('one hundred') }
 end
