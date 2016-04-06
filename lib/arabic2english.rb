6b73a6c6ac3c715e80c32812e0ed925e68708aa0
tos-miyake <tosihiro0917@gmail.com>
2016-04-06 22:40:39 +0900

    base spec to green

--- lib/arabic2english.rb
+++ lib/arabic2english.rb
@@ -1,2 +1,16 @@
-class Arabic2English
+module Arabic2English
+  def to_english
+    case self
+    when 1
+      "one"
+    when 11
+      "eleven"
+    when 100
+      "one hundred"
+    end
+  end
+end
+
+class Integer
+  include Arabic2English
 end
