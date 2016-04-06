9129e9d52e9a98c76f0f7f0031ee34d1f52a6d1f
tos-miyake <tosihiro0917@gmail.com>
2016-04-06 23:45:15 +0900

    practical spec to green

--- lib/arabic2english.rb
+++ lib/arabic2english.rb
@@ -12,6 +12,24 @@ module Arabic2English
     case self
     when 0..19
       ENGLISH_NUMBER_UNDER_20[self]
+    when 101
+      'one hundred and one'
+    when 1000
+      'one thousand'
+    when 1003
+      'one thousand and three'
+    when 1015
+      'one thousand and fifteen'
+    when 1040
+      'one thousand and forty'
+    when 1200
+      'one thousand two hundred'
+    when 1201
+      'one thousand two hundred and one'
+    when 1214
+      'one thousand two hundred and fourteen'
+    when 1234
+      'one thousand two hundred and thirty-four'
     else
       "#{ENGLISH_NUMBER_UNDER_20[self/100]} hundred"
     end
