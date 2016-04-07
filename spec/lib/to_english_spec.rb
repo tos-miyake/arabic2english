07f34bb49d72803b49ee33b6046af0197995f6e7
tos-miyake <tosihiro0917@gmail.com>
2016-04-07 21:57:18 +0900

    add spec under 100numbers

--- spec/lib/to_english_spec.rb
+++ spec/lib/to_english_spec.rb
@@ -6,4 +6,9 @@ describe ToEnglish do
   it { expect(1.to_english).to eq('one') }
   it { expect(3.to_english).to eq('three') }
   it { expect(11.to_english).to eq('eleven') }
+
+  it { expect(20.to_english).to eq('twenty') }
+  it { expect(21.to_english).to eq('twenty-one') }
+  it { expect(45.to_english).to eq('forty-five') }
+  it { expect(80.to_english).to eq('eighty') }
 end
