37c7197145cc26ed60ddde8902fa3e1231c4cacd
tos-miyake <tosihiro0917@gmail.com>
2016-04-07 22:19:08 +0900

    add spec under 1000 numbers

--- spec/lib/to_english_spec.rb
+++ spec/lib/to_english_spec.rb
@@ -11,4 +11,13 @@ describe ToEnglish do
   it { expect(21.to_english).to eq('twenty-one') }
   it { expect(45.to_english).to eq('forty-five') }
   it { expect(80.to_english).to eq('eighty') }
+
+  it { expect(100.to_english).to eq('one hundred') }
+  it { expect(119.to_english).to eq('one hundred and nineteen') }
+  it { expect(221.to_english).to eq('two hundred and twenty-one') }
+  it { expect(545.to_english).to eq('five hundred and forty-five') }
+  it { expect(800.to_english).to eq('eight hundred') }
+  it { expect(901.to_english).to eq('nine hundred and one') }
+  it { expect(980.to_english).to eq('nine hundred and eighty') }
+  it { expect(999.to_english).to eq('nine hundred and ninety-nine') }
 end
