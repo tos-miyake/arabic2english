95553af6eb3c36bc17e7ed9240f550748bbf4660
tos-miyake <tosihiro0917@gmail.com>
2016-04-07 23:11:40 +0900

    refactor to_english for under 1_000 numbers

--- lib/to_english.rb
+++ lib/to_english.rb
@@ -7,22 +7,8 @@ module ToEnglish
       CONVERT_UNDER_20[self]
     when 20..99
       under100_to_english
-    when 100
-      'one hundred'
-    when 119
-      'one hundred and nineteen'
-    when 221
-      'two hundred and twenty-one'
-    when 545
-      'five hundred and forty-five'
-    when 800
-      'eight hundred'
-    when 901
-      'nine hundred and one'
-    when 980
-      'nine hundred and eighty'
-    when 999
-      'nine hundred and ninety-nine'
+    when 100..999
+      under1_000_to_english self
     end
   end
 
@@ -35,6 +21,19 @@ module ToEnglish
       CONVERT_10_DIGIT[_10digit]
     else
       "#{CONVERT_10_DIGIT[_10digit]}-#{CONVERT_UNDER_20[_1digit]}"
+    end
+  end
+
+  def under1_000_to_english num
+    _100digit_english = CONVERT_UNDER_20[num / 100]
+    under_100_english = nil
+    unless (under100 = (num % 100)).zero?
+     under_100_english =  under100.to_english
+    end
+    if under_100_english
+      "#{_100digit_english} hundred and #{under_100_english}"
+    else
+      "#{_100digit_english} hundred"
     end
   end
 end
