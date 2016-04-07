6333791affdee099bf3adbcae4dd0880485aeafc
tos-miyake <tosihiro0917@gmail.com>
2016-04-07 21:44:08 +0900

    add under20 spec

--- spec/lib/to_english_spec.rb
+++ spec/lib/to_english_spec.rb
@@ -1,5 +1,9 @@
 require 'to_english'
 
 describe ToEnglish do
-  it 'to_english'
+  it { expect(1).to respond_to(:to_english) }
+
+  it { expect(1.to_english).to eq('one') }
+  it { expect(3.to_english).to eq('three') }
+  it { expect(11.to_english).to eq('eleven') }
 end
