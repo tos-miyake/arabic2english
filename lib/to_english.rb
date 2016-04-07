75a18d9f035a82a7f41bfb2f383fa575ed51c79e
tos-miyake <tosihiro0917@gmail.com>
2016-04-07 22:01:41 +0900

    under100 numbers spec to green

--- lib/to_english.rb
+++ lib/to_english.rb
@@ -2,7 +2,18 @@ require 'to_english/constant'
 
 module ToEnglish
   def to_english
-    CONVERT_UNDER_20[self]
+    case self
+    when 0..19
+      CONVERT_UNDER_20[self]
+    when 20
+      'twenty'
+    when 21
+      'twenty-one'
+    when 45
+      'forty-five'
+    when 80
+      'eighty'
+    end
   end
 end
 
